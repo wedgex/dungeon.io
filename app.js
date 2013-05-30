bf7fa979a55ea9b701117f951f51a0ef98eee2e8
Hunter Haydel <haydh530@gmail.com>
2013-05-30 18:49:27 -0500

    basic express structure

---
+++ app.js
@@ -0,0 +1,11 @@
+var express = require("express");
+
+var app  = express();
+
+app.use(express.static(__dirname + "/public"));
+
+app.get("/", function(request, response) {
+	response.render('index.html');
+});
+
+app.listen(process.env.PORT || 8383);
